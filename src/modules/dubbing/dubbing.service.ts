4c8e4814baf0270a2a480182e1066c52f69d728c
liangqi <1739484858@qq.com>
2025-11-25 10:03:57 +0800

    feat: 添加配音的一些操作

--- src/modules/dubbing/dubbing.service.ts
+++ src/modules/dubbing/dubbing.service.ts
@@ -92,6 +92,9 @@ export class DubbingService {
     return { timeline };
   }
 
+  // 先生成material
+  // 再生成一个track
+  // track里面有一个segment对应material
   async addDubbing(
     draftId: string,
     audioInfos: { filePath: string; fileName: string; duration_us: number }[],
@@ -108,5 +111,130 @@ export class DubbingService {
       '../../__templates__/audio/audio.track.json',
     );
     const trackInfoTemplate = await this.file.readJson(trackInfoTemplatePath);
+  }
+
+  // 生成material
+  async addMaterial({ filePath, fileName, draftId }): Promise<{
+    material_id: string;
+  }> {
+    const baseMaterialInfoPath = path.resolve(
+      __dirname,
+      '../../__templates__/audio/audio.material.json',
+    );
+
+    const baseMaterialInfo = await this.file.readJson(baseMaterialInfoPath);
+    const material_id = randomUUID();
+    const materialInfo = {
+      id: material_id,
+      name: fileName,
+      path: ConfigKeys.JIANYING_DIR + '/' + filePath,
+    };
+    const mergedMaterialInfo = { ...baseMaterialInfo, ...materialInfo };
+
+    const draftInfoPath = path.resolve(
+      process.cwd(),
+      'public',
+      'drafts',
+      draftId,
+      'draft_info.json',
+    );
+    const draft_info = await this.file.readJson(draftInfoPath);
+
+    draft_info.materials.audios.push(mergedMaterialInfo);
+
+    await this.file.writeJson(draftInfoPath, draft_info);
+
+    return {
+      material_id,
+    };
+  }
+
+  // 生成track
+  async addTrack(draftId): Promise<{
+    track_id: string;
+  }> {
+    const trackInfoPath = path.resolve(
+      __dirname,
+      '../../__templates__/audio/audio.track.json',
+    );
+
+    const trackInfo = await this.file.readJson(trackInfoPath);
+
+    const track_id = randomUUID();
+
+    trackInfo.id = track_id;
+
+    const draftInfoPath = path.resolve(
+      process.cwd(),
+      'public',
+      'drafts',
+      draftId,
+      'draft_info.json',
+    );
+
+    const draft_info = await this.file.readJson(draftInfoPath);
+
+    draft_info.tracks.push(trackInfo);
+
+    await this.file.writeJson(draftInfoPath, draft_info);
+
+    return {
+      track_id,
+    };
+  }
+
+  // 生成segment
+  async addSegment(
+    draftId,
+    trackId,
+    material_id,
+    fileInfo: { duration_us: number },
+  ): Promise<{
+    segment_id: string;
+  }> {
+    const baseSegmentInfoPath = path.resolve(
+      __dirname,
+      '../../__templates__/audio/audio.track.json',
+    );
+
+    const baseSegmentInfo = await this.file.readJson(baseSegmentInfoPath);
+
+    const draftInfoPath = path.resolve(
+      process.cwd(),
+      'public',
+      'drafts',
+      draftId,
+      'draft_info.json',
+    );
+
+    const draft_info = await this.file.readJson(draftInfoPath);
+
+    const track = draft_info.tracks.find((r) => r.id === trackId);
+
+    const lastSegment = track.segments[track.segments.length - 1];
+
+    const segment_id = randomUUID();
+    const info = {
+      id: segment_id,
+      material_id,
+      source_timerange: {
+        duration: fileInfo.duration_us,
+        start: 0,
+      },
+      target_timerange: {
+        duration: fileInfo.duration_us,
+        start: lastSegment.start + lastSegment.duration,
+      },
+    };
+
+    const mergedSegmentInfo = { ...baseSegmentInfo, ...info };
+
+    track.segments.push(mergedSegmentInfo);
+
+    await this.file.writeJson(draftInfoPath, draft_info);
+
+    return {
+      segment_id,
+    };
   }
 }
